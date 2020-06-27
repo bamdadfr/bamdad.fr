a35a8150e83c631fdc6e8bedf1add2004b14f0fd
Bamdad Sabbagh <git@bamdadsabbagh.com>
2020-06-27 03:30:59 +0200

    fix: we getting there

--- test.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-if ./check.sh nginx:alpine docker.pkg.github.com/bamdadsabbagh/bamdadsabbagh-www/bamdadsabbagh-www:latest; then
-    echo "images are up to date"
-else
-    echo "image needs an update"
-fi
