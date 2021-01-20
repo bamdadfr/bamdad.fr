7f006baa2d179d17398168deba0f49334ef1e95c
Bamdad Sabbagh <git@bamdadsabbagh.com>
2021-01-20 07:33:00 +0100

    fix: removed useless service worker

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,5 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import * as serviceWorker from './config/serviceWorker'
 import 'bulma/css/bulma.min.css'
 import { AppStyle } from './components/app.style'
 import { App } from './components/app'
@@ -16,5 +15,3 @@ ReactDOM.render (
     <Root />,
     document.getElementById ('root'),
 )
-
-serviceWorker.register ()
