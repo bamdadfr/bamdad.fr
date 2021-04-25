fe5c4efe45c99acd3166d992b98006a44f10fe07
Bamdad Sabbagh <git@bamdadsabbagh.com>
2021-04-25 13:10:21 +0200

    fix(layout-languages): moved business logic to their respective hooks

--- src/components/layout-languages/layout-languages.component.js
+++ src/components/layout-languages/layout-languages.component.js
@@ -1,90 +1,46 @@
-import React, { useEffect, useState } from 'react'
+import React, { useState } from 'react'
 import AnimationSlideComponent from '@/components/animation-slide/animation-slide.component'
 import { LanguagesData } from '@/data/languages.data'
 import AnimationFadeComponent from '@/components/animation-fade/animation-fade.component'
+import styled from 'styled-components'
+import { useInitialDisplay } from '@/hooks/use-initial-display'
+import { useIterateLanguages } from '@/hooks/use-iterate-languages'
 import { StyledIcon, StyledText, StyledTextContainer } from './layout-languages.styles'
+
+export const StyledContainer = styled.div`
+    display: ${props => props.display};
+`
 
 export default function LayoutLanguagesComponent () {
 
-    const [index, setIndex] = useState (0)
-    const [isVisible, setIsVisible] = useState (true)
-
-    useEffect (() => {
-
-        function nextIndex () {
-
-            if (typeof LanguagesData[index + 1] === 'undefined') {
-
-                return setIndex (0)
-
-            }
-
-            return setIndex (index + 1)
-
-        }
-
-        new Promise (
-            (resolve) => {
-
-                setTimeout (() => {
-
-                    setIsVisible (false)
-
-                    resolve ()
-
-                }, 2400)
-
-            },
-        )
-            .then (
-                () => new Promise (
-                    (resolve) => {
-
-                        setTimeout (() => {
-
-                            nextIndex ()
-
-                            resolve ()
-
-                        }, 1200)
-
-                    },
-                ),
-            )
-            .then (
-                () => new Promise (
-                    (resolve) => {
-
-                        setTimeout (() => {
-
-                            setIsVisible (true)
-
-                            resolve ()
-
-                        }, 0)
-
-                    },
-                ),
-            )
-
-    }, [index, LanguagesData.length])
+    const [initialDelay] = useState (8600)
+    const [initialDisplay] = useInitialDisplay (initialDelay)
+    const [{ index, isVisible }] = useIterateLanguages (LanguagesData.length, initialDisplay)
 
     return (
         <>
-            <AnimationFadeComponent delay={8600}>
-                <AnimationFadeComponent delay={350} isVisible={isVisible}>
-                    <StyledIcon color={LanguagesData[index].color}>
-                        {LanguagesData[index].icon}
-                    </StyledIcon>
-                </AnimationFadeComponent>
-                <StyledTextContainer>
-                    <AnimationSlideComponent isVisible={isVisible}>
-                        <StyledText>
-                            {LanguagesData[index].text}
-                        </StyledText>
-                    </AnimationSlideComponent>
-                </StyledTextContainer>
-            </AnimationFadeComponent>
+            {
+                initialDisplay
+                    ?
+                    (
+                        <>
+                            <AnimationFadeComponent delay={350} isVisible={isVisible}>
+                                <StyledIcon color={LanguagesData[index].color}>
+                                    {LanguagesData[index].icon}
+                                </StyledIcon>
+                            </AnimationFadeComponent>
+                            <StyledTextContainer>
+                                <AnimationSlideComponent isVisible={isVisible}>
+                                    <StyledText>
+                                        {LanguagesData[index].text}
+                                    </StyledText>
+                                </AnimationSlideComponent>
+                            </StyledTextContainer>
+                        </>
+                    )
+                    :
+                    null
+            }
             <AnimationSlideComponent delay={7600}>
                 I spend my time with
             </AnimationSlideComponent>
