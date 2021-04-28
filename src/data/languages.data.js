bccc8540654c06976781165e4518f7e840f2c77e
Bamdad Sabbagh <git@bamdadsabbagh.com>
2021-04-28 13:08:56 +0200

    feat(languages): cleaned languages, added capitalization + new languages

--- src/data/languages.data.js
+++ src/data/languages.data.js
@@ -2,24 +2,24 @@ import React from 'react'
 import { ShuffleUtils } from '@/utils/shuffle.utils'
 import { Icon } from '@iconify/react'
 import ansibleIcon from '@iconify/icons-simple-icons/ansible'
-import archlinuxIcon from '@iconify/icons-simple-icons/archlinux'
 import centosIcon from '@iconify/icons-simple-icons/centos'
 import debianIcon from '@iconify/icons-simple-icons/debian'
 import dockerIcon from '@iconify/icons-simple-icons/docker'
 import githubActionsIcon from '@iconify/icons-simple-icons/githubactions'
 import gnuBashIcon from '@iconify/icons-simple-icons/gnubash'
 import grafanaIcon from '@iconify/icons-simple-icons/grafana'
+import javascriptIcon from '@iconify/icons-simple-icons/javascript'
 import laravelIcon from '@iconify/icons-simple-icons/laravel'
-import magentoIcon from '@iconify/icons-simple-icons/magento'
 import mongodbIcon from '@iconify/icons-simple-icons/mongodb'
 import mysqlIcon from '@iconify/icons-simple-icons/mysql'
+import nextdotjsIcon from '@iconify/icons-simple-icons/next-dot-js'
 import nodeDotJs from '@iconify/icons-simple-icons/node-dot-js'
 import ovhIcon from '@iconify/icons-simple-icons/ovh'
 import phpIcon from '@iconify/icons-simple-icons/php'
 import prometheusIcon from '@iconify/icons-simple-icons/prometheus'
 import pythonIcon from '@iconify/icons-simple-icons/python'
 import reactIcon from '@iconify/icons-simple-icons/react'
-import redisIcon from '@iconify/icons-simple-icons/redis'
+import sassIcon from '@iconify/icons-simple-icons/sass'
 import scalewayIcon from '@iconify/icons-simple-icons/scaleway'
 import typescriptIcon from '@iconify/icons-simple-icons/typescript'
 import webpackIcon from '@iconify/icons-simple-icons/webpack'
@@ -31,107 +31,107 @@ import webpackIcon from '@iconify/icons-simple-icons/webpack'
  */
 export const LanguagesData = ShuffleUtils ([
     {
-        'text': 'ansible',
+        'text': 'Ansible',
         'icon': <Icon icon={ansibleIcon}/>,
         'color': '#1A1918',
     },
     {
-        'text': 'arch linux',
-        'icon': <Icon icon={archlinuxIcon}/>,
-        'color': '#1793D1',
-    },
-    {
-        'text': 'centos',
+        'text': 'CentOS',
         'icon': <Icon icon={centosIcon}/>,
         'color': '#932178',
     },
     {
-        'text': 'debian',
+        'text': 'Debian',
         'icon': <Icon icon={debianIcon}/>,
         'color': '#D70751',
     },
     {
-        'text': 'docker',
+        'text': 'Docker',
         'icon': <Icon icon={dockerIcon}/>,
         'color': '#2496ED',
     },
     {
-        'text': 'github actions',
+        'text': 'GitHub Actions',
         'icon': <Icon icon={githubActionsIcon}/>,
         'color': '#2088FF',
     },
     {
-        'text': 'gnu bash',
+        'text': 'GNU Bash',
         'icon': <Icon icon={gnuBashIcon}/>,
         'color': '#2B3539',
     },
     {
-        'text': 'grafana',
+        'text': 'Grafana',
         'icon': <Icon icon={grafanaIcon}/>,
         'color': '#F05A28',
     },
     {
-        'text': 'laravel',
+        'text': 'JavaScript',
+        'icon': <Icon icon={javascriptIcon}/>,
+        'color': '#f0db4f',
+    },
+    {
+        'text': 'Laravel',
         'icon': <Icon icon={laravelIcon}/>,
         'color': '#FF291A',
     },
     {
-        'text': 'php',
+        'text': 'PHP',
         'icon': <Icon icon={phpIcon}/>,
         'color': '#777BB3',
     },
     {
-        'text': 'magento',
-        'icon': <Icon icon={magentoIcon}/>,
-        'color': '#F2631C',
-    },
-    {
-        'text': 'mongodb',
+        'text': 'MongoDB',
         'icon': <Icon icon={mongodbIcon}/>,
         'color': '#3FA037',
     },
     {
-        'text': 'mysql',
+        'text': 'MySQL',
         'icon': <Icon icon={mysqlIcon}/>,
         'color': '#00618A',
     },
     {
-        'text': 'nodejs',
+        'text': 'Next.js',
+        'icon': <Icon icon={nextdotjsIcon}/>,
+        'color': '#000000',
+    },
+    {
+        'text': 'Node.js',
         'icon': <Icon icon={nodeDotJs}/>,
         'color': '#68A063',
     },
     {
-        'text': 'ovh',
+        'text': 'OVH',
         'icon': <Icon icon={ovhIcon}/>,
         'color': '#03429A',
     },
     {
-        'text': 'prometheus',
+        'text': 'Prometheus',
         'icon': <Icon icon={prometheusIcon}/>,
         'color': '#ff4646',
     },
     {
-        'text': 'python',
+        'text': 'Python',
         'icon': <Icon icon={pythonIcon}/>,
         'color': '#FFD03D',
     },
     {
-        'text': 'react',
+        'text': 'React',
         'icon': <Icon icon={reactIcon}/>,
         'color': '#60DBFB',
     },
     {
-        'text': 'redis',
-        'icon': <Icon icon={redisIcon}/>,
-        'color': '#D82C20',
+        'text': 'Sass',
+        'icon': <Icon icon={sassIcon}/>,
+        'color': '#cc6699',
     },
     {
-        'text': 'scaleway',
+        'text': 'Scaleway',
         'icon': <Icon icon={scalewayIcon}/>,
         'color': '#510098',
     },
     {
-        'text': 'typescript',
+        'text': 'TypeScript',
         'icon': <Icon icon={typescriptIcon}/>,
         'color': '#007acc',
     },
