4bdd2e62db6472c6d9f46cef271d0b8e19fc83d4
Bamdad Sabbagh <git@bamdadsabbagh.com>
2021-07-19 23:07:34 +0200

    chore: add jest config

--- jest.config.js
+++ jest.config.js
@@ -1,1 +1,13 @@
-module.exports = {}
+module.exports = {
+    'setupFilesAfterEnv': ['./jest.setup.js'],
+    'testRegex': '(test|spec)\\.(.*\\.)?[jt]sx?$',
+    'moduleNameMapper': {
+        '^.+.(css|styl|less|sass|scss|png|jpg|ttf|woff|woff2)$': 'jest-transform-stub',
+    },
+    'testEnvironment': 'jsdom',
+    'modulePathIgnorePatterns': [
+        '<rootDir>/.next/',
+    ],
+    'collectCoverage': true,
+    'coverageDirectory': './coverage/',
+}
