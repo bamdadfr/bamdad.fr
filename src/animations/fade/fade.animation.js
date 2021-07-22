6190e84a751ca8177b9ff3611f907b958e9c6868
Bamdad Sabbagh <git@bamdadsabbagh.com>
2021-07-22 18:48:24 +0200

    perf(animations): replace `react-spring` with `@react-spring/web`

--- src/animations/fade/fade.animation.js
+++ src/animations/fade/fade.animation.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import PropTypes from 'prop-types'
-import { animated, useSpring } from 'react-spring'
+import { animated, useSpring } from '@react-spring/web'
 
 const propTypes = {
     'children': PropTypes.node.isRequired,
