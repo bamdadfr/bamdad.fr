27797dd65fd56cb21510797fb86e42f809c60e31
Bamdad Sabbagh <hi@bamdad.fr>
2021-12-06 22:17:57 +0100

    feat: change languages

--- src/components/languages/languages.constants.js
+++ src/components/languages/languages.constants.js
@@ -1,83 +1,29 @@
 import React from 'react';
 import {Icon} from '@iconify/react';
-import debianIcon from '@iconify/icons-simple-icons/debian';
-import dockerIcon from '@iconify/icons-simple-icons/docker';
-import githubActionsIcon from '@iconify/icons-simple-icons/githubactions';
-import gnuBashIcon from '@iconify/icons-simple-icons/gnubash';
-import grafanaIcon from '@iconify/icons-simple-icons/grafana';
-import javascriptIcon from '@iconify/icons-simple-icons/javascript';
-import jestIcon from '@iconify/icons-simple-icons/jest';
-import mongodbIcon from '@iconify/icons-simple-icons/mongodb';
-import mysqlIcon from '@iconify/icons-simple-icons/mysql';
-import nestjsIcon from '@iconify/icons-simple-icons/nestjs';
-import nextdotjsIcon from '@iconify/icons-simple-icons/next-dot-js';
-import nodeDotJs from '@iconify/icons-simple-icons/node-dot-js';
-import reactIcon from '@iconify/icons-simple-icons/react';
-import sassIcon from '@iconify/icons-simple-icons/sass';
 import typescriptIcon from '@iconify/icons-simple-icons/typescript';
+import reactIcon from '@iconify/icons-simple-icons/react';
+import nextdotjsIcon from '@iconify/icons-simple-icons/nextdotjs';
+import reduxIcon from '@iconify/icons-simple-icons/redux';
+import jestIcon from '@iconify/icons-simple-icons/jest';
+import testingLibraryIcon from '@iconify/icons-simple-icons/testinglibrary';
+import cypressIcon from '@iconify/icons-simple-icons/cypress';
+import puppeteerIcon from '@iconify/icons-simple-icons/puppeteer';
 import webpackIcon from '@iconify/icons-simple-icons/webpack';
+import babelIcon from '@iconify/icons-simple-icons/babel';
+import viteIcon from '@iconify/icons-simple-icons/vite';
+import githubActionsIcon from '@iconify/icons-simple-icons/githubactions';
+import immerIcon from '@iconify/icons-simple-icons/immer';
+import reactivexIcon from '@iconify/icons-simple-icons/reactivex';
+import d3dotjsIcon from '@iconify/icons-simple-icons/d3dotjs';
+import threedotjsIcon from '@iconify/icons-simple-icons/threedotjs';
+import gitIcon from '@iconify/icons-simple-icons/git';
 import {shuffleArray} from '../../utils/shuffle-array/shuffle-array';
 
 export const LanguagesConstants = shuffleArray([
   {
-    'text': 'Debian',
-    'icon': <Icon icon={debianIcon} />,
-    'color': '#D70751',
-  },
-  {
-    'text': 'Docker',
-    'icon': <Icon icon={dockerIcon} />,
-    'color': '#2496ED',
-  },
-  {
-    'text': 'GitHub Actions',
-    'icon': <Icon icon={githubActionsIcon} />,
-    'color': '#2088FF',
-  },
-  {
-    'text': 'GNU Bash',
-    'icon': <Icon icon={gnuBashIcon} />,
-    'color': '#2B3539',
-  },
-  {
-    'text': 'Grafana',
-    'icon': <Icon icon={grafanaIcon} />,
-    'color': '#F05A28',
-  },
-  {
-    'text': 'JavaScript',
-    'icon': <Icon icon={javascriptIcon} />,
-    'color': '#f0db4f',
-  },
-  {
-    'text': 'Jest',
-    'icon': <Icon icon={jestIcon} />,
-    'color': '#99425B',
-  },
-  {
-    'text': 'MongoDB',
-    'icon': <Icon icon={mongodbIcon} />,
-    'color': '#3FA037',
-  },
-  {
-    'text': 'MySQL',
-    'icon': <Icon icon={mysqlIcon} />,
-    'color': '#00618A',
-  },
-  {
-    'text': 'NestJS',
-    'icon': <Icon icon={nestjsIcon} />,
-    'color': '#E0234E',
-  },
-  {
-    'text': 'Next.js',
-    'icon': <Icon icon={nextdotjsIcon} />,
-    'color': '#000000',
-  },
-  {
-    'text': 'Node.js',
-    'icon': <Icon icon={nodeDotJs} />,
-    'color': '#68A063',
+    'text': 'TypeScript',
+    'icon': <Icon icon={typescriptIcon} />,
+    'color': '#007acc',
   },
   {
     'text': 'React',
@@ -85,18 +31,78 @@ export const LanguagesConstants = shuffleArray([
     'color': '#60DBFB',
   },
   {
-    'text': 'Sass',
-    'icon': <Icon icon={sassIcon} />,
-    'color': '#cc6699',
+    'text': 'Next.js',
+    'icon': <Icon icon={nextdotjsIcon} />,
+    'color': '#000000',
   },
   {
-    'text': 'TypeScript',
-    'icon': <Icon icon={typescriptIcon} />,
-    'color': '#007acc',
+    'text': 'Redux',
+    'icon': <Icon icon={reduxIcon} />,
+    'color': '#764abc',
+  },
+  {
+    'text': 'Jest',
+    'icon': <Icon icon={jestIcon} />,
+    'color': '#99425B',
+  },
+  {
+    'text': 'Testing Library',
+    'icon': <Icon icon={testingLibraryIcon} />,
+    'color': '#DB2F2F',
+  },
+  {
+    'text': 'Cypress',
+    'icon': <Icon icon={cypressIcon} />,
+    'color': '#3C3C3C',
+  },
+  {
+    'text': 'Puppeteer',
+    'icon': <Icon icon={puppeteerIcon} />,
+    'color': '#4FD7A2',
   },
   {
     'text': 'webpack',
     'icon': <Icon icon={webpackIcon} />,
     'color': '#1C78C0',
+  },
+  {
+    'text': 'Babel',
+    'icon': <Icon icon={babelIcon} />,
+    'color': '#F9DC3D',
+  },
+  {
+    'text': 'Vite',
+    'icon': <Icon icon={viteIcon} />,
+    'color': '#F9D32C',
+  },
+  {
+    'text': 'GitHub Actions',
+    'icon': <Icon icon={githubActionsIcon} />,
+    'color': '#2088FF',
+  },
+  {
+    'text': 'Immer',
+    'icon': <Icon icon={immerIcon} />,
+    'color': '#00E7C3',
+  },
+  {
+    'text': 'RxJS',
+    'icon': <Icon icon={reactivexIcon} />,
+    'color': '#F4218A',
+  },
+  {
+    'text': 'D3',
+    'icon': <Icon icon={d3dotjsIcon} />,
+    'color': '#F79144',
+  },
+  {
+    'text': 'Three.js',
+    'icon': <Icon icon={threedotjsIcon} />,
+    'color': '#000000',
+  },
+  {
+    'text': 'Git',
+    'icon': <Icon icon={gitIcon} />,
+    'color': '#E94E31',
   },
 ]);
