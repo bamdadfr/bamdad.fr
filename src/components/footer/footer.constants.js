3e65e7a33629177d471543343a56b544f57a5b4e
Bamdad Sabbagh <hi@bamdad.fr>
2022-01-06 09:35:08 +0100

    feat: remove linkedin

--- src/components/footer/footer.constants.js
+++ src/components/footer/footer.constants.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import {Icon} from '@iconify/react';
-import linkedinIcon from '@iconify/icons-simple-icons/linkedin';
 import githubIcon from '@iconify/icons-simple-icons/github';
 
 export const FooterConstants = [
@@ -9,11 +8,5 @@ export const FooterConstants = [
     href: 'https://github.com/bamdadsabbagh',
     icon: <Icon icon={githubIcon} />,
     color: '#24292e',
-  },
-  {
-    text: 'linkedin',
-    href: 'https://www.linkedin.com/in/bamdadsabbagh/',
-    icon: <Icon icon={linkedinIcon} />,
-    color: '#0077b5',
   },
 ];
