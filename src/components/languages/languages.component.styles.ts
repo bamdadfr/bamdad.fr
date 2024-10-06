8470543712678a10090344ace749d4987713846b
Bamdad Sabbagh <hi@bamdad.fr>
2024-10-06 15:14:39 +0200

    feat: use languages part as h2

--- src/components/languages/languages.component.styles.ts
+++ src/components/languages/languages.component.styles.ts
@@ -1,6 +1,6 @@
 import styled from '@emotion/styled';
 
-export const Container = styled.div`
+export const Container = styled.h2`
   display: flex;
   flex-direction: column;
   justify-content: flex-end;
